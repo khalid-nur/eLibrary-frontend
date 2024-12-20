0799f45cd1d547361885cb8c8c6beefaad204f11
khalid-nur <khalidnur150@gmail.com>
2024-12-20 17:42:38 -0600

    add contact us section for users to reach out with questions

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -5,6 +5,7 @@ import BookCollection from "./component/BookCollection";
 import WhyChooseUs from "./component/WhyChooseUs";
 import HowItWorks from "./component/HowItWorks";
 import AboutUs from "./component/AboutUs";
+import ContactUs from "./component/ContactUs";
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <WhyChooseUs />
       <HowItWorks />
       <AboutUs />
+      <ContactUs />
     </>
   );
 };
